63699bf1aa819a3a37f3ea6f529dd330d3f8d211
”Ivan <ivan.spasov203@gmail.com>
2024-06-10 00:09:44 +0700

    Initial commit

---
+++ src/main/java/com/spasov/planplus/PlanPlusApplication.java
@@ -0,0 +1,13 @@
+package com.spasov.planplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanPlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spasov/planplus/PlanPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spasov.planplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
